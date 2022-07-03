6d467705cfe3665a6484870e01fd862d463494d6
Nadiia Sahanenko <nadinvgs@gmail.com>
2022-07-03 16:54:59 +0300

    notes

--- WorkSpace/Controllers/WorkSpaceController.cs
+++ WorkSpace/Controllers/WorkSpaceController.cs
@@ -18,6 +18,7 @@ namespace WorkSpace.Controllers
         [HttpGet]
         public async Task<ActionResult<IQueryable<Object>>> GetAllWorkSpace()
         {
+            //Массив воркспейсов(id, name)
             //return IQueryable<GetAllWorkSpaceResponse>
             return null;
         }
@@ -26,6 +27,7 @@ namespace WorkSpace.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Object>> GetWorkSpaceById(int id)
         {
+            //list pages(id, name, date)
             //return Object <GetWorkSpaceResponse>
             //response ActionResult 400 incorrect Id
             //response ActionResult 404 with such id was not found.
@@ -34,8 +36,9 @@ namespace WorkSpace.Controllers
 
         // PUT: api/workspaces/5
         [HttpPut("{id}")]
-        public async Task<ActionResult> ChangeWorkSpaceById(int id)
+        public async Task<ActionResult> ChangeWorkSpaceById(int id, object workSpace)
         {
+            //update only name
             //response ActionResult OK 204
             //response ActionResult 400 incorrect Id
             //response ActionResult 404 with such id was not found.
